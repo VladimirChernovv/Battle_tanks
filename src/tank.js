44857f219934e951efd503e88e4fcc6b626f158b
Vladimir Chernov <dgudo73@mail.ru>
2021-08-23 13:49:54 +0300

    Сократили код отвечающий за передвижение и анимацию танка

--- src/tank.js
+++ src/tank.js
@@ -18,5 +18,26 @@ export default class Tank {
 
   get sprite() {
     return this.frames[this.direction * 2 + this.animationFrame];
-  }
+  };
+
+  // метод принимающий нажатые ключи
+  update(activeKeys) {
+    if (activeKeys.has('ArrowUp')) {
+      this._move(0, 'y', -1)
+    } else if (activeKeys.has('ArrowRight')) {
+      this._move(1, 'x', 1);
+    } else if (activeKeys.has('ArrowDown')) {
+      this._move(2, 'y', 1);
+    } else if (activeKeys.has('ArrowLeft')) {
+      this._move(3, 'x', -1);
+    }
+  };
+
+
+  _move(direction, axis, value) {
+    this.direction = direction;
+    this[axis] += value;
+    this.animationFrame ^= 1;
+  };
+
 }
